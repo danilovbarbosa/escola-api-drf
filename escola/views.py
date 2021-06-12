3730282b18b8129165bf44bd01e62e8f12d271a8
Danilo Costa <danilo.barbosa@fapce.edu.br>
2021-06-12 18:34:35 -0300

    refactor(black): corrigido o pep8.

--- escola/views.py
+++ escola/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     context = {}
-    return render(request, '', context=context)
+    return render(request, "", context=context)
