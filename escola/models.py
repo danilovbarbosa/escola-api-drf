503870bf0a58715ad63dd6a2f5646486bfd751ab
Danilo Costa <danilo.barbosa@fapce.edu.br>
2021-06-12 19:05:20 -0300

    feat(escola/models): inserida classe Aluno.

--- escola/models.py
+++ escola/models.py
@@ -6,7 +6,22 @@ class Aluno(models.Model):
     nome = models.CharField(max_length=50)
     rg = models.CharField(max_length=11)
     cpf = models.CharField(max_length=11)
-    data_de_nascimento = models.DateField(auto_now=True)
-    
+    data_de_nascimento = models.DateField()
+
+    def __str__(self):
+        return self.nome
+
+
+class Curso(models.Model):
+    NIVEL = (
+        ("B", "Básico"),
+        ("I", "Intermediário"),
+        ("A", "Avançado"),
+    )
+
+    codigo_curso = models.CharField(max_length=50)
+    descricao = models.CharField(max_length=11)
+    nivel = models.CharField(max_length=11)
+
     def __str__(self):
         return self.nome
