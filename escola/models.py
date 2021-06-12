517904f3a851d44fa4da5456446609285f68056f
Danilo Costa <danilo.barbosa@fapce.edu.br>
2021-06-12 18:48:49 -0300

    feat(escola/models): inserida classe Aluno.

--- escola/models.py
+++ escola/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Aluno(models.Model):
+    nome = models.CharField(max_length=50)
+    rg = models.CharField(max_length=11)
+    cpf = models.CharField(max_length=11)
+    data_de_nascimento = models.DateField(auto_now=True)
+    
+    def __str__(self):
+        return self.nome
