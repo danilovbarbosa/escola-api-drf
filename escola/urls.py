655c020d610464982c3c7f51b4574906869b8e4c
Danilo Costa <danilo.barbosa@fapce.edu.br>
2021-06-17 22:08:49 -0300

    feat(escola/urls): adicionada rota.

--- escola/urls.py
+++ escola/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
-from escola.views import AlunoViewSet, CursoViewSet
+from escola.views import AlunoViewSet, CursoViewSet, MatriculaViewSet
 
 router = routers.DefaultRouter()
 router.register("alunos", AlunoViewSet, basename="Alunos")
 router.register("cursos", CursoViewSet, basename="Cursos")
+router.register("matriculas", MatriculaViewSet, basename="Matriculas")
 
 
 urlpatterns = [
